1fc29454089aed8b25b2efc6dca9b622be5567d0
Denis <d.limarev@corp.mail.ru>
2021-10-24 15:15:53 +0300

    defer in loop

--- projects/packr/builder/builder.go
+++ projects/packr/builder/builder.go
@@ -75,7 +75,7 @@ func (b *Builder) Run() error {
 }
 
 func (b *Builder) dump() error {
-	for _, p := range b.pkgs {
+	dumpPkg := func(p pkg) error {
 		name := filepath.Join(p.Dir, "a_"+p.Name+"-packr.go")
 		f, err := os.Create(name)
 		defer f.Close()
@@ -89,6 +89,13 @@ func (b *Builder) dump() error {
 		}
 		err = t.Execute(f, p)
 		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	for _, p := range b.pkgs {
+		if err := dumpPkg(p); err != nil {
 			return err
 		}
 	}
